833a7376409f6fe5e2b7d739d47fe78d41c7d633
masterUNG <phrombutr@gmail.com>
2015-10-19 16:01:52 +0700

    Finish Day1

--- app/src/main/java/appewtc/masterung/baacrestaurant/MainActivity.java
+++ app/src/main/java/appewtc/masterung/baacrestaurant/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     //Explicit
     private UserTABLE objUserTABLE;
+    private FoodTABLE objFoodTABLE;
 
 
     @Override
@@ -17,11 +18,20 @@ public class MainActivity extends AppCompatActivity {
         //Create & Connected Database
         createAndConnected();
 
+        //Tester Add New Value
+        testerAdd();
+
     }   // Main Method
+
+    private void testerAdd() {
+        objUserTABLE.addNewUser("testUser", "testPassword", "ทดสอบชื่อภาษาไทย");
+        objFoodTABLE.addNewFood("ชื่ออาหาร", "testSource", "123");
+    }
 
     private void createAndConnected() {
 
         objUserTABLE = new UserTABLE(this);
+        objFoodTABLE = new FoodTABLE(this);
 
     }
 
