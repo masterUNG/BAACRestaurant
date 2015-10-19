e7046def77c32b113254d8de0159ad458a499d9f
masterUNG <phrombutr@gmail.com>
2015-10-19 13:02:11 +0700

    Start Create OpenHelper

---
+++ app/src/main/java/appewtc/masterung/baacrestaurant/MyOpenHelper.java
@@ -0,0 +1,20 @@
+package appewtc.masterung.baacrestaurant;
+
+import android.content.Context;
+
+/**
+ * Created by masterUNG on 10/19/15 AD.
+ */
+public class MyOpenHelper {
+
+    //Explicit
+    private static final String DATABASE_NAME = "BAAC.db";
+    private static final int DATABASE_VERSION = 1;
+
+
+
+
+    public MyOpenHelper(Context context) {
+    }   // Constructor
+
+}   // Main Class นี่คือ คลาสหลัก นะจร้า
