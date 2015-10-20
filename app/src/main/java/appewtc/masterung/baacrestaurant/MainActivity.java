f3fc9565d1995a5bf3aa56661fa2efa7386ca618
masterUNG <phrombutr@gmail.com>
2015-10-20 11:02:38 +0700

    Delete All SQLite Success

--- app/src/main/java/appewtc/masterung/baacrestaurant/MainActivity.java
+++ app/src/main/java/appewtc/masterung/baacrestaurant/MainActivity.java
@@ -1,8 +1,13 @@
 package appewtc.masterung.baacrestaurant;
 
 import android.database.sqlite.SQLiteDatabase;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import org.apache.http.client.methods.HttpPost;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,7 +30,42 @@ public class MainActivity extends AppCompatActivity {
         //Delete All SQLite
         deleteAllSQLite();
 
+        //Synchronize JSON to SQLite
+        synJSONtoSQLite();
+
+
     }   // Main Method
+
+    private void synJSONtoSQLite() {
+
+        //0. Change Policy
+        StrictMode.ThreadPolicy myPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(myPolicy);
+
+
+        int intTimes = 0;
+        while (intTimes <= 1) {
+
+            InputStream objInputStream = null;
+            String strJSON = null;
+            String strUserURL = "http://swiftcodingthai.com/baac/php_get_data_master.php";
+            String strFoodURL = "http://swiftcodingthai.com/baac/php_get_food.php";
+            HttpPost objHttpPost;
+
+            //1. Create InputStream
+
+
+            //2. Create JSON String
+
+
+            //3. Update SQLite
+
+
+            intTimes += 1;
+        }   // while
+
+
+    }   // synJSONtoSQLite
 
     private void deleteAllSQLite() {
 
